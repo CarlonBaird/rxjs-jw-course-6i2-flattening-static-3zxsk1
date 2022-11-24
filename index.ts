3a1609b62736e4668ae42091a5dc5e09c530cdb5
CarlonBaird <carlyestrella@live.com>
2022-11-24 15:50:03 -0400

    1 file modified

--- index.ts
+++ index.ts
@@ -9,7 +9,34 @@ const source$ = new Observable((subscriber) => {
 console.log('App has started');
 source$
   .pipe(
-    //emit the values from another observable into this observable
+    //
     concatMap((value) => of(1, 2))
   )
   .subscribe((value) => console.log(value));
+
+/* Explanation:
+
+    So in our case, concatMap's logic will subscribe to the Observable created using the 'of' function, over
+
+here.
+
+The values emitted by this inner Subscription to this inner Observable will then be flattened to
+
+the output.
+
+So our Observer will just receive those two next notifications with values '1' and '2' without knowing
+
+that there were some additional Subscriptions happening above.
+
+And later on, after three more seconds, value 'B' will be emitted by the source and this next notification
+
+will, once again, reach the concatMap's logic, which will again call the function over here and create
+
+an inner Subscription to the returned 'of' Observable. And once again, the emitted values
+
+'1' and '2' will be handled by the concatMap's logic, which will reemit them to the output.
+
+As a side note, you may have noticed that we don't use the value emitted by the source Observable,
+
+and this is to keep things as simple as possible in this example.
+*/
