115ba43cfebfe3fcfd8055f2f9cf9350a94c9b5a
CarlonBaird <carlyestrella@live.com>
2022-11-24 15:42:40 -0400

    1 file modified

--- index.ts
+++ index.ts
@@ -1,3 +1,15 @@
-import { Observable } from 'rxjs';
+import { Observable, of } from 'rxjs';
+import { concatMap } from 'rxjs/operators';
 
-const source$ = new Observable((subscriber) => {});
+const source$ = new Observable((subscriber) => {
+  setTimeout(() => subscriber.next('A'), 2000);
+  setTimeout(() => subscriber.next('B'), 5000);
+});
+
+console.log('App has started');
+source$
+  .pipe(
+    //emit the values from another observable into this observable
+    concatMap((value) => of(1, 2))
+  )
+  .subscribe((value) => console.log(value));
